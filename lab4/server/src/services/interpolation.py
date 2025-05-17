9fdad3dd1203a01bd1c7b17942e9e0bd131568e8
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-05-17 18:55:00 +0300

    ✨ feat(server): round coefficients

--- lab4/server/src/services/interpolation.py
+++ lab4/server/src/services/interpolation.py
@@ -1,5 +1,4 @@
-import time
-
+import sympy as sp
 from libs.interpolation.methods import (
     LagrangeSolver,
     NewtonDividedDifferencesSolver,
@@ -21,6 +20,8 @@ from schemas.interpolation import (
     PointInterpolationResponse,
 )
 
+x = sp.symbols("x")
+
 
 class InterpolationService:
     async def interpolate(self, data: InterpolationRequest) -> InterpolationResponse:
@@ -40,7 +41,7 @@ class InterpolationService:
         res_data: InterpolationData | None = None
         if validation_res.success:
             res = solver.solve()
-            f_expr = str(res.expr)
+            f_expr = round_expr(res.expr)
             res_data = InterpolationData(f_expr=f_expr)
         return InterpolationResponse(
             method=solver.interpolation_method,
@@ -50,7 +51,9 @@ class InterpolationService:
             data=res_data,
         )
 
-    async def point_interpolate(self, data: PointInterpolationRequest) -> PointInterpolationResponse:
+    async def point_interpolate(
+        self, data: PointInterpolationRequest
+    ) -> PointInterpolationResponse:
         solver: BasePointSolver | None = None
 
         if data.method == PointInterpolationMethod.STIRLING:
@@ -63,7 +66,7 @@ class InterpolationService:
         res_data: PointInterpolationData | None = None
         if validation_res.success:
             res = solver.solve()
-            f_expr = str(res.expr)
+            f_expr = round_expr(res.expr)
             res_data = PointInterpolationData(f_expr=f_expr, y_value=res.y_value)
 
         return PointInterpolationResponse(
@@ -74,3 +77,32 @@ class InterpolationService:
             message=validation_res.message,
             data=res_data,
         )
+
+
+def round_expr(expr: sp.Expr, n: int = 10) -> str:
+    def round_floats(x):
+        if isinstance(x, sp.Float):
+            return sp.Float(round(x, n))
+        elif isinstance(x, sp.core.numbers.Integer):
+            return x
+        elif isinstance(
+            x,
+            (
+                sp.core.numbers.Rational,
+                sp.core.numbers.Pi,
+                sp.core.numbers.E,
+                sp.core.numbers.EulerGamma,
+            ),
+        ):
+            return x
+        elif isinstance(x, sp.core.power.Pow):
+            return x.func(round_floats(x.base), x.exp)
+        return x
+
+    # Create a mapping of all float numbers to their rounded versions
+    mapping = {x: round_floats(x) for x in expr.atoms(sp.Float)}
+
+    # Substitute all floats in the expression with their rounded versions
+    rounded_expr = expr.subs(mapping)
+
+    return str(rounded_expr)
